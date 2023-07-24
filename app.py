d620fd7261dae3d69d98a651769598ecb13f49a8
Kshitiz <kshitiz.khandelwal16@gmail.com>
2023-07-24 18:37:54 +0530

    not req

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if uploaded_file is not None:
         col1, col2, col3, col4 = st.columns(4)
 
         with col1:
-            st.header("Total Messages")
+            st.header("total Messages")
             st.title(num_messages)
         with col2:
             st.header("Total Words")
